98e3e75bc76dc89f71c5bac242bffda447da95b4
Anas Samoudi <unlimiteddez@gmail.com>
2022-12-01 03:46:10 +0100

    made the sidebar fixed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Rightbar from "./Rightbar/Rightbar";
 import Sidebar from "./Sidebar/Sidebar";
 
 const Layout = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(true);
     return (
-        <div className="flex relative">
-            <Sidebar />
+        <div className="flex relative justify-start">
+            <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
             <Rightbar />
-            <div className="px-8 py-8 w-full">
+            <div
+                className={`ml-16 px-8 py-8 w-full ${
+                    isOpen ? "sm:ml-80" : "sm:mt-16"
+                }`}
+            >
                 <Outlet />
             </div>
         </div>
